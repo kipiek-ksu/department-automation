030374773e6addd9bb2552df991e3fc46a397317
Denis Senchishen <dsenchishen@icloud.com>
2022-09-01 22:49:10 +0300

    feat(script): update repos automation script

--- repository-management/scripts.py
+++ repository-management/scripts.py
@@ -44,7 +44,9 @@ TEMPLATE_REPO = 'term-paper-template'
 #         repos[-1]['repo'] = f'term-paper-{repos[-1]["year"]}-{repos[-1]["name"].lower()}-{repos[-1]["surname"].lower()}'
 #         repos[-1]['description'] = f'Term paper of {repos[-1]["name"]} {repos[-1]["surname"]} ' \
 #                                    f'for {repos[-1]["year"]} year'
-#
+
+###############################################################################
+# CREATE REPO
 # for repo in repos:
 #     result = requests.post(
 #         f'https://api.github.com/repos/{TEMPLATE_OWNER}/{TEMPLATE_REPO}/generate',
@@ -56,7 +58,27 @@ TEMPLATE_REPO = 'term-paper-template'
 #         "include_all_branches":false,
 #         "private":false}}'''
 #     )
-#     print(repo['repo'], result.status_code)
+#     print('CREATE REPO', repo['repo'], result.status_code)
+
+###############################################################################
+# SET TOPICS
+# for repo in repos:
+#     result = requests.put(
+#         f'https://api.github.com/repos/{ORG_NAME}/{repo["repo"]}/topics',
+#         headers=HEADERS,
+#         data=f'''{{"names":["{repo['year']}","term-paper"]}}'''
+#     )
+#     print('UPDATE TOPICS', repo['repo'], result.status_code)
+
+###############################################################################
+# INVITE USER
+# for repo in repos:
+#     result = requests.put(
+#         f'https://api.github.com/repos/{ORG_NAME}/{repo["repo"]}/collaborators/{repo["username"]}',
+#         headers=HEADERS,
+#         data='{"permission":"write"}'
+#     )
+#     print('ADD COLLABORATOR', repo['repo'], result.status_code)
 #
 # print()
 
@@ -68,6 +90,6 @@ TEMPLATE_REPO = 'term-paper-template'
 #         f'https://api.github.com/repos/{ORG_NAME}/{repo["repo"]}',
 #         headers=HEADERS
 #     )
-#     print(repo['repo'], result.status_code)
-#
-# print()
+#     print('DELETE REPO', repo['repo'], result.status_code)
+
+print()
